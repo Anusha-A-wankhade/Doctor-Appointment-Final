1788be382c4b13f1924380940c419af7b4fb1023
Anusha <anushawankhade12345@gmail.com>
2023-04-09 15:15:53 +0800

    Hi all, in this project I have provided the section of the top doctor list like cardiologists, neurologists, etc with their experience in their field and the place of their clinic which help them to find doctors easily and to book an appointment (in the future scope). And in the menu in future I will provide a section on Medicine Description in which people allow to upload their prescription in file format and the medicine delivery will come in their contact also a section on youtube videos of yoga, exercise, and meditation which help them to stay fit and Happy.

---
+++ app/src/main/java/com/example/doctorappointment3/DashboardActivity2.java
@@ -0,0 +1,13 @@
+package com.example.doctorappointment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashboardActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard2);
+    }
+}
